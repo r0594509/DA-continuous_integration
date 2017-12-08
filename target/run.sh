78e4b7c54ec7612cb16df8f348cf0d458c66b5ae
Nathan Olmanst <nat@olmanst.net>
2017-12-08 13:30:44 +0100

    removed script file

--- target/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-echo "********************************************************"
-echo "Starting License Server"
-echo "********************************************************"
-java -jar /usr/local/licensingservice/licensing-service-0.0.1-SNAPSHOT.jar
